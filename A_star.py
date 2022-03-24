742ae4ab41caa36f18f09288c94748082106ceff
youssef hassan <youssefhassan369@yahoo.com>
2022-03-24 18:50:25 +0200

    main and A* modified

--- A_star.py
+++ A_star.py
@@ -2,10 +2,13 @@ from search import *
 import time
 import heapq
 import math
+
+
 def takeSecond(list):
     return list[1]
 
-def A_star(boardstate):
+
+def A_star(boardstate, heuristic):
     maxDep = 0
     Start_time = time.time()
     frontier = []
@@ -13,26 +16,29 @@ def A_star(boardstate):
     explored = set()
     frontier.append([boardstate, 0])
     oFrontier.add(boardstate)
-    pMap={boardstate: boardstate}
+    pMap = {boardstate: boardstate}
     while len(frontier):
         frontier.sort(key=takeSecond)
         state = frontier.pop(0)  # Pop first element from the list (to imitate queue)
         oFrontier.remove(state[0])  # Remove the same element from the optimizing frontier set
         explored.add(state[0])
         if isGoal(state[0]):  # if current state is the goal state --> break
+            #printBoard(state[0])
             break
         children = getChildren(state[0])  # Get all children of current state
         for child in children:
             if child not in oFrontier and child not in explored:  # search for each child in frontier and explored
                 maxDep = max(maxDep,state[1])  # maximum depth is the max of current max depth or level of current state
-                frontier.append([child,manhattanHeuristic(state[0])])  # if not found add child to frontier and oFrontier
+                if heuristic == 'manhattan':
+                    frontier.append([child, manhattanHeuristic(state[0])])  # if not found add child to frontier and oFrontier
+                elif heuristic == 'euclidean':
+                    frontier.append([child, euclideanHeuristic(state[0])])  # if not found add child to frontier and oFrontier
                 oFrontier.add(child)
                 pMap[child] = state[0]  # Store the child with the current state as the parent node
     end_time = time.time()  # Ending Time
     path = findPath(pMap)  # Traverse through the dictionary to find the path of the goal state
     path.reverse()
     for i in range(len(path)):  # Print each state in the path
-        print(f'Step Number: {i + 1}')
         printBoard(path[i])
     print(f'Cost of Path = {len(path) - 1}')  # cost of path equal number of state changes in path to goal state
     print(f'Number of Nodes Expanded = {len(explored)}')
--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 from search import *
 from A_star import *
 
-print('How would you like to initialize the puzzle ?')
-print('1) Enter a puzzle to solve')
-print('2) initialize random puzzle')
-initialize =input('Choice: ')
-if initialize == '1':
-    randomState =input('Puzzle: ')
-elif initialize == '2':
-    randomState = generateRandomState()
+while (True):
+    print('How would you like to initialize the puzzle ?')
+    print('1) Enter a puzzle to solve')
+    print('2) initialize random puzzle')
+    initialize = input('Choice: ')
+    if initialize == '1':
+        randomState = input('Puzzle: ')
+    elif initialize == '2':
+        randomState = generateRandomState()
 
-print('Solving: ')
-printBoard(randomState)
-if not isSolvable(randomState):
-    print('This state is Unsolvable! (Odd number of inversions)')
-else:
-    print('Select Method:')
-    print('1) BFS')
-    print('2) DFS')
-    print('3) A* (Manhattan Distance Heuristic)')
-    print('4) A* (Euclidean Distance Heuristic)')
-    choice = input('Choice: ')
-    if choice == '1':
-        BFS(randomState)
-    elif choice == '2':
-        DFS(randomState)
-    elif choice == '3':
-        A_star(randomState)
+    print('Solving: ')
+    printBoard(randomState)
+    if not isSolvable(randomState):
+        print('This state is Unsolvable! (Odd number of inversions)')
     else:
-        print('')
+        print('Select Method:')
+        print('1) BFS')
+        print('2) DFS')
+        print('3) A* (Manhattan Distance Heuristic)')
+        print('4) A* (Euclidean Distance Heuristic)')
+        choice = input('Choice: ')
+        if choice == '1':
+            BFS(randomState)
+        elif choice == '2':
+            DFS(randomState)
+        elif choice == '3':
+            A_star(randomState, heuristic='manhattan')
+        else:
+            A_star(randomState, heuristic='euclidean')
